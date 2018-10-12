eadea6c20893af96ed88d0ca13babcb4cd114893
lolicnea <loliconkea@gmail.com>
2018-10-12 12:03:09 +0800

    finish take function1

--- test.js
+++ test.js
@@ -5,5 +5,14 @@ describe('take', function () {
   it("_.take(['1', '2', '3']) equalTo ['1']", function () {
     assert.deepStrictEqual(take(['1', '2', '3']), ['1'])
   })
+  it("_.take(['1', '2', '3'],2) equalTo ['1', '2']", function () {
+    assert.deepStrictEqual(take(['1', '2', '3'],2), ['1', '2'])
+  })
+  it("_.take(['1', '2', '3'],5) equalTo ['1', '2','3']", function () {
+    assert.deepStrictEqual(take(['1', '2', '3'],5), ['1', '2','3'])
+  })
+  it("_.take(['1', '2', '3'],0) equalTo []", function () {
+    assert.deepStrictEqual(take(['1', '2', '3'],0), [])
+  })
   
 })
